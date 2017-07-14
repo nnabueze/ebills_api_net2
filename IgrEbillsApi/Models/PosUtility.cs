4a94db60372f6c5f27cdd3cbf71edcd5d4fc6f23
Opara Nnabueze <villavelle101@gmail.com>
2017-07-14 11:10:01 +0100

    added pos utility and pos dtos

---
+++ IgrEbillsApi/Models/PosUtility.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace IgrEbillsApi.Models
+{
+    public class PosUtility
+    {
+        private IgrAdo _db;
+
+        public PosUtility()
+        {
+            _db = new IgrAdo();
+        }
+
+        protected void Dispose()
+        {
+            _db.Dispose();
+        }
+    }
+}
