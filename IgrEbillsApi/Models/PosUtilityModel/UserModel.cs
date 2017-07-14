980e60304816fd1ae39e7d6193297ce6e6736ac7
Opara Nnabueze <villavelle101@gmail.com>
2017-07-14 14:42:54 +0100

    added pos utility model folder

---
+++ IgrEbillsApi/Models/PosUtilityModel/UserModel.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace IgrEbillsApi.Models.PosUtilityModel
+{
+    public class UserModel
+    {
+        [Required]
+        public string Phone { get; set; }
+
+        [Required]
+        public string Pin { get; set; }
+
+        [Required]
+        public string ActivationCode { get; set; }
+    }
+}
