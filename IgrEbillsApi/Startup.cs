d802eee97e519c5a56492d535807be5db84116c5
Opara Nnabueze <villavelle101@gmail.com>
2017-07-14 04:02:18 +0100

    token base authentication woring

--- IgrEbillsApi/Startup.cs
+++ IgrEbillsApi/Startup.cs
@@ -20,6 +20,7 @@ namespace IgrEbillsApi
 
             HttpConfiguration config = new HttpConfiguration();
             WebApiConfig.Register(config);
+            app.UseCors(Microsoft.Owin.Cors.CorsOptions.AllowAll);
             app.UseWebApi(config);
         }
 
