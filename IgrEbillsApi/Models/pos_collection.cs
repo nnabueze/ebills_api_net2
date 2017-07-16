376d7842afaa6520edd709c4d77a498e7dd26787
Opara Nnabueze <villavelle101@gmail.com>
2017-07-16 02:56:52 +0100

    added pos poco clas

---
+++ IgrEbillsApi/Models/pos_collection.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace IgrEbillsApi.Models
+{
+    public class pos_collection
+    {
+        [Key]
+        [StringLength(38)]
+        public string COLLECTION_ID { get; set; }
+
+        public decimal Amount { get; set; }
+
+        [StringLength(38)]
+        public string Phone { get; set; }
+
+        [StringLength(200)]
+        public string Name { get; set; }
+
+        [StringLength(38)]
+        public string USER_ID { get; set; }
+
+        [StringLength(38)]
+        public string remittance_id { get; set; }
+
+        [StringLength(38)]
+        public string MDA_ID { get; set; }
+
+        [StringLength(38)]
+        public string MDAStation_ID { get; set; }
+
+        [StringLength(38)]
+        public string SubHead_ID { get; set; }
+
+        [StringLength(38)]
+        public string POS_ID { get; set; }
+
+        [StringLength(200)]
+        public string Email { get; set; }
+
+        public DateTime? create_at { get; set; }
+
+        public DateTime? updated_at { get; set; }
+    }
+}
